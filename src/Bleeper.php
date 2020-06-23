81f4d814cce6ceb6eaca93f0f3ab46d6520e3c77
carlos alva sandoval <carlos.alva@ivisa.com>
2020-06-23 00:13:36 -0500

    remove typing parameters and regenerateToken function

--- src/Bleeper.php
+++ src/Bleeper.php
@@ -8,14 +8,14 @@ class Bleeper
 	public $baseUrl = 'http://localhost:3000';
 	public $apiKey;
 
-	public function __construct(string $apiKey)
+	public function __construct($apiKey)
 	{
 		$this->apiKey = $apiKey;
 	}
 
-	public function messageList(string $cellphone, string $token, int $currentPage, int $pageSize)
+	public function messageList($cellphone, $token, $currentPage, $pageSize)
 	{
-		$url = $this->baseUrl . '/api/message/'.$currentPage.'/'.$pageSize;
+		$url = $this->baseUrl . '/api/message/' . $currentPage . '/' . $pageSize;
 		$client = new GuzzleClient();
 		$response = $client->request('GET', $url, [
 			'headers' => [
@@ -23,13 +23,13 @@ class Bleeper
 				'Authorization' => 'Bearer ' . $token,
 			],
 			'form_params' => [
-				'cellphone' => $cellphone
-			]
+				'cellphone' => $cellphone,
+			],
 		]);
 		return $this->getBodyResponse($response);
 	}
 
-	public function sendMessage(string $token, string $from, string $to, string $message, string $attachment_url = null, $saveLocal = false)
+	public function sendMessage($token, $from, $to, $message, $attachment_url = null, $saveLocal = false)
 	{
 		$url = $this->baseUrl . '/api/send_message';
 		$client = new GuzzleClient();
@@ -41,9 +41,9 @@ class Bleeper
 			'form_params' => [
 				'from' => $from,
 				'message' => $message,
-				'attachment_url'=>  $attachment,
+				'attachment_url' => $attachment,
 				'to' => $to,
-				'save_local'=> $saveLocal
+				'save_local' => $saveLocal,
 			],
 		]);
 		return $this->getBodyResponse($response);
@@ -56,15 +56,27 @@ class Bleeper
 		$response = $client->request('POST', $url, [
 			'headers' => [
 				'Content-Type' => 'application/x-www-form-urlencoded',
-				'Authorization' => 'Beader '.$this->apiKey
-			]
+				'Authorization' => 'Beader ' . $this->apiKey,
+			],
 		]);
 		return $this->getBodyResponse($response);
+	}
+
+	public function regenerateTokenOnExpiration($token)
+	{
+		$tokenParts = explode('.', $token);
+		$payloadEncoded = $tokenParts[1];
+		$payload = json_decode(base64_decode($payloadEncoded), true);
+		$expirationDate = $payload['expiration_date'];
+		$now = strtotime(date()) * 1000;
+		if ($now >= $expirationDate)
+		{
+			$this->getToken();
+		}
 	}
 
 	private function getBodyResponse($response)
 	{
 		return json_decode($response->getBody()->getContents(), true);
 	}
-
 }
